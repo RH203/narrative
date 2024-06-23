23dd28902a87bb0a12c8beed1fbe10353ef2f3fa
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-24 00:32:32 +0700

    chore: Refactor InfoCard component
    The InfoCard component has been refactored to improve code readability and remove unused prop types. This ensures that the component is more maintainable and efficient.

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,26 +1,25 @@
 import propTypes from "prop-types";
 
-const InfoCard = ({img, title, desc, style}) => {
+const InfoCard = ({ img, title, desc }) => {
   return (
-    <div>
-      <img src={img} className={`${style}`}/>
-      <h6>
+    <div className="flex justify-center items-center flex-col gap-2 border border-gray-300 py-3 group cursor-pointer hover:bg-red-500 transition duration-500">
+      <div className="group-hover:bg-slate-200 rounded-full px-2 py-3 transition duration-500">
+        {img}
+      </div>
+      <h6 className="font-bold group-hover:text-white transition duration-500">
         {title}
       </h6>
-      <small>
+      <small className="group-hover:text-white transition duration-500">
         {desc}
       </small>
     </div>
-  )
-}
-
+  );
+};
 
 InfoCard.propTypes = {
   img: propTypes.any,
   title: propTypes.string,
   desc: propTypes.string,
-  style: propTypes.string,
 };
-
 
 export default InfoCard;
