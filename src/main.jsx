0b8983ec4c25ee3ceba415cb9def93885e233981
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-06-23 16:15:01 +0700

    chore: Update main.jsx to use react-router-dom and redux for routing and state management

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import App from "./App";
+
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 );
